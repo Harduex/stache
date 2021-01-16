8ce772598217ad27bcd6421a306b13ac4aef7176
Harduex <simeon.simka@gmail.com>
2021-01-16 02:09:07 +0200

    Some panini syntax demonstration added in new list component

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,1 @@
-console.log("Hello from main"),console.log("Hello from component"),console.log("Hello from header");
+console.log("Hello from main"),console.log("Hello from component"),console.log("Hello from header"),console.log("Hello from component");
