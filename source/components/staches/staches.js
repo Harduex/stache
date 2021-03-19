0909861d077985dd8159aa6d43f36e3e0d22a62c
simeon <simeon.simka@gmail.com>
2021-03-19 15:27:56 +0200

    Added validation to check if env is node for helpers, wrapper component example created

--- source/components/staches/staches.js
+++ source/components/staches/staches.js
@@ -1,10 +1,18 @@
-module.exports = function (options) {
-    var staches = `
-    <div class="staches">
-        <img class="stache__img--left" src="images/stache-logo-left.png" alt="{">
-        <p class="${options.hash.class}">${options.fn(this)}</p>
-        <img class="stache__img--right" src="images/stache-logo-right.png" alt="}">
-    </div>
-    `;
-    return staches;
+if ((typeof process !== 'undefined') && (typeof process.versions.node !== 'undefined')) {
+
+    module.exports = (options) => {
+        const children = options.fn(this);
+        const { className } = options.hash;
+
+        return (
+            `<div class="staches">
+                <img class="stache__img--left" src="images/stache-logo-left.png" alt="{">
+                    <p class="${className}">
+                        ${children}
+                    </p>
+                <img class="stache__img--right" src="images/stache-logo-right.png" alt="}">
+            </div>`
+        );
+    }
+
 }
