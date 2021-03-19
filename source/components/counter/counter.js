47923ac116db14cc660babc8545c669ed50f134e
simeon <simeon.simka@gmail.com>
2021-03-19 21:42:46 +0200

    Fixed counter not proper reset bug, more fixes to helpers

--- source/components/counter/counter.js
+++ source/components/counter/counter.js
@@ -1,61 +1,68 @@
-// * Counter component logic
-const Counter = (function (global) {
-    const inputElement = ".counter__input";
-    const numberElement = ".counter__number";
+//code run by browser   
+if (typeof window !== "undefined" && typeof window.document !== "undefined") {
+    // * Counter component logic
+    const Counter = (function (global) {
+        const inputElement = ".counter__input";
+        const numberElement = ".counter__number";
 
-    let value = 0;
-    let number = Number($(inputElement).val() ?? 0);
+        let value = 0;
+        let number = Number($(inputElement).val() ?? 0);
 
 
-    return (actions) => {
+        return (actions) => {
 
-        // * Setup function that runs by default on Counter component function invoking
-        function load() {
-            // * Some function calls
-            handleInputChange();
-            console.log("Counter Loaded");
-        }
+            // * Setup function that runs by default on Counter component function invoking
+            function load() {
+                // * Some function calls
+                handleInputChange();
+                console.log("Counter Loaded");
+            }
 
-        // * Actions
-        function add() {
-            value += number;
-            $(numberElement).html(value);
+            // * Actions
+            function add() {
+                value += number;
+                $(numberElement).html(value);
+            };
+
+            function sub() {
+                value -= number;
+                $(numberElement).html(value);
+            };
+
+            function reset() {
+                value = 0;
+                number = 1;
+                $(numberElement).html(value);
+                $(inputElement).val(value + 1);
+            };
+
+            // * Event listeners
+            function handleInputChange() {
+                $(inputElement).on('change paste keyup', function () {
+                    var value = $(this).val();
+                    number = Number($(this).val());
+                    $(this).val(value.replace(/[^\d.-]/g, ''));
+                })
+            }
+
+            // * Calling specific action
+            switch (actions) {
+                case 'add':
+                    return add();
+                case 'sub':
+                    return sub();
+                case 'reset':
+                    return reset();
+                default:
+                    load();
+            }
         };
 
-        function sub() {
-            value -= number;
-            $(numberElement).html(value);
-        };
+    }(window))
 
-        function reset() {
-            value = 0;
-            $(numberElement).html(value);
-            $(inputElement).val(value + 1);
-        };
+    Counter();
 
-        // * Event listeners
-        function handleInputChange() {
-            $(inputElement).on('change paste keyup', function () {
-                var value = $(this).val();
-                number = Number($(this).val());
-                $(this).val(value.replace(/[^\d.-]/g, ''));
-            })
-        }
-
-        // * Calling specific action
-        switch (actions) {
-            case 'add':
-                return add();
-            case 'sub':
-                return sub();
-            case 'reset':
-                return reset();
-            default:
-                load();
-        }
-    };
-
-}(window))
-
-Counter();
-
+} else {
+    // code run by node
+    // Use for helpers
+}
--- source/components/layouts/footer/footer.js
+++ source/components/layouts/footer/footer.js
@@ -1,26 +1,29 @@
-const Footer = (function (global) {
-    const yearElement = '.footer__copyright-year';
-    const currentYear = new Date().getFullYear();
+//Code run by browser
+if (typeof window !== "undefined" && typeof window.document !== "undefined") {
+    const Footer = (function (global) {
+        const yearElement = '.footer__copyright-year';
+        const currentYear = new Date().getFullYear();
 
-    return (actions) => {
+        return (actions) => {
 
-        function load() {
-            updateFooterYear();
-            console.log('Footer loaded');
-        }
+            function load() {
+                updateFooterYear();
+                console.log('Footer loaded');
+            }
 
-        function updateFooterYear() {
-            $(yearElement).text(currentYear);
+            function updateFooterYear() {
+                $(yearElement).text(currentYear);
+            };
+
+            switch (actions) {
+                case 'updateFooterYear':
+                    return updateFooterYear();
+                default:
+                    load();
+            }
         };
 
-        switch (actions) {
-            case 'updateFooterYear':
-                return updateFooterYear();
-            default:
-                load();
-        }
-    };
+    }(window))
 
-}(window))
-
-Footer();
+    Footer();
+}
